d4ee710be552f0806ff6b07e667049ac9166ef76
Allhush <hushagenalliam@gmail.com>
2026-07-12 21:02:13 -0600

    made some desicions going forward, still sticking to previously outlined plan, just going to learn some things first to make it go faster

--- test7.py
+++ test7.py
@@ -3,7 +3,7 @@ import random
 alphabet_no_symbol = ["a", "b", "c", "d", "e", "f", "g", "h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 #currently not useful, original intended role is fufilled by alphabet_no_symbol, but it may be useful in the future so it will remain here for the time being
-#keypressed = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+keypressed = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 
 rotor1 = [[4, 10, 12, 5, 11, 6, 3, 16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9],[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]]
 rotor2 = [[0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4],[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]]
@@ -13,6 +13,8 @@ rotor5 = [[21, 25, 1, 17, 6, 8, 19, 24, 20, 15, 18, 3, 13, 7, 11, 23, 0, 22, 12,
 rotor6 = [[9, 15, 6, 21, 14, 20, 12, 5, 24, 16, 1, 4, 13, 7, 25, 17, 3, 10, 0, 18, 23, 11, 8, 2, 19, 22],[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]]
 rotor7 = [[13, 25, 9, 7, 6, 17, 2, 23, 12, 24, 18, 22, 1, 14, 20, 5, 0, 8, 21, 11, 15, 4, 10, 16, 3, 19],[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]]
 rotor8 = [[5, 10, 16, 7, 19, 11, 23, 14, 2, 1, 9, 18, 15, 3, 25, 17, 0, 12, 4, 22, 13, 8, 20, 24, 6, 21],[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]]
+
+
 
 message = "shush"
 
@@ -42,4 +44,12 @@ print(number_list);
 #was going to try to use classes but I can't remeber how to use them
 #fuck it I will just hard code this shit and then turn it into a class later
 
+#I regretted this immediately and am currently trying to relearn how to make classes
+
 rotors_used = [rotor1, rotor2, rotor3, rotor4]
+
+for i in number_list:
+    z = keypressed.index(i);
+
+
+    
